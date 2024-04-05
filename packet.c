d9446dbb27696b2f2936b0cee107cc9d8e8ff10c
Your Name <you@example.com>
2024-04-05 23:51:25 +0900

    assignment

--- packet.c
+++ packet.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <arpa/inet.h>
-#include "myheader.h" 
+#include "myheader.h"
 
-// 패킷 캡처 콜백 함수
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     struct ethheader *eth = (struct ethheader *)packet;
 
-    // 이더넷 헤더 출력
     printf("Ethernet Header:\n");
     printf("Destination MAC: ");
     for (int i = 0; i < 6; i++) {
@@ -24,7 +22,6 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 
     if (ntohs(eth->ether_type) == 0x0800) { // IPv4
         struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
-        // IP 헤더 출력
         printf("IP Header:\n");
         printf("Version: %d\n", ip->iph_ver);
         printf("Header Length: %d\n", ip->iph_ihl);
@@ -42,7 +39,6 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
         switch (ip->iph_protocol) {
             case IPPROTO_TCP: {
                 struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + sizeof(struct ipheader));
-                // TCP 헤더 출력
                 printf("TCP Header:\n");
                 printf("Source Port: %d\n", ntohs(tcp->tcp_sport));
                 printf("Destination Port: %d\n", ntohs(tcp->tcp_dport));
@@ -66,7 +62,6 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
             }
             case IPPROTO_ICMP: {
                 struct icmpheader *icmp = (struct icmpheader *)(packet + sizeof(struct ethheader) + sizeof(struct ipheader));
-                // ICMP 헤더 출력
                 printf("ICMP Header:\n");
                 printf("Type: %d\n", icmp->icmp_type);
                 printf("Code: %d\n", icmp->icmp_code);
@@ -85,29 +80,28 @@ int main() {
     pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE];
     struct bpf_program fp;
-    char filter_exp[] = "ip"; // 필터 표현식
+    char filter_exp[] = "ip";
     bpf_u_int32 net;
 
-    // Step 1: NIC의 이름이 enp0s3인 라이브 pcap 세션을 열기
+    // Step 1: Open live pcap session on NIC with name enp0s3
     handle = pcap_open_live("enp0s25", BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device enp0s3: %s\n", errbuf);
         exit(EXIT_FAILURE);
     }
 
-    // Step 2: 필터 표현식을 BPF 가상 코드로 컴파일
+    // Step 2: Compile filter_exp into BPF psuedo-code
     if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
         fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
         exit(EXIT_FAILURE);
     }
 
-    // 컴파일된 필터를 세션에 설정
     if (pcap_setfilter(handle, &fp) != 0) {
         pcap_perror(handle, "Error:");
         exit(EXIT_FAILURE);
     }
 
-    // Step 3: 패킷 캡처 시작
+    // Step 3: Capture packets
     pcap_loop(handle, -1, got_packet, NULL);
 
     pcap_close(handle);
